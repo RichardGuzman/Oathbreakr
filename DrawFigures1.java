be10e35ce2fcf0a1cc87511f8efd1e8f05a7c863
RichardGuzman <richard.guzman@techhigh.us>
2014-09-19 10:12:48 -0400

    This is my final product.

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,34 +1,55 @@
-public class DrawFigures1 {
-    public static void main(String[] args) {
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println();
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println();
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+")
-        System.out.println("|United|");
-        System.out.println("|States|");
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-    }
-}
+
+   public class DrawFigure3 {
+      public static void main(String[] args) {
+	  DrawDiamond();
+	  DrawX();
+	  DrawRocket();
+	  } 
+	  public static void DrawDiamond(){
+	  drawcone();
+	  drawV();
+	  System.out.println();
+	  }
+	  
+	  public static void DrawX(){
+	  drawV();
+	  drawcone();
+	  System.out.println();
+	  }
+	  
+	  public static void DrawRocket(){
+	  drawcone();
+	  drawBox();
+	  System.out.println("|United|");
+	  System.out.println("|States|");
+	  drawBox();
+	  drawcone();
+	  System.out.println();
+	  }
+	
+	  public static void drawcone (){
+	   System.out.println("   /\\");
+      System.out.println("  /  \\");
+      System.out.println(" /    \\");
+	  
+	  } 
+	  
+	  public static void drawV (){
+	   System.out.println(" \\    /");
+      System.out.println("  \\  /");
+      System.out.println("   \\/");
+      
+	  }
+	  
+	  public static void drawBox (){
+	   System.out.println("+------+");
+      System.out.println("|      |");
+      System.out.println("|      |");
+      System.out.println("+------+");
+	  
+	
+	
+	    }
+	}
+
+    
