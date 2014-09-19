5105d1baad951e6425fc41efe14771ece6381c80
RichardGuzman <richard.guzman@techhigh.us>
2014-09-19 10:05:29 -0400

    This is my DrawFigures1 with comments.

---
+++ DrawFigures1.java
@@ -0,0 +1,39 @@
+public class DrawFigures1 {
+    public static void main(String[] args) {
+	 // Everything here is made up of three basic shapes and some text.
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+		  //The cone above and the V bellow are two shapes we'll be needing so those can be used.
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println();
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+		  //notice the cone and the V are again used both above and bellow.
+        System.out.println();
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+		  //the square is another shape you will be seeing.
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+")
+		  // bellow is the text that we'll be using, so you can put that inbetween the two squares like so. "|TEXT|"
+        System.out.println("|United|");
+        System.out.println("|States|");
+        System.out.println("+------+");//the other square 
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("   /\\");// and the final cone.
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+    }
+}
